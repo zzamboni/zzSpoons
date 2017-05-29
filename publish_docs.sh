c4c65b243eb6a4ad0fda1e9a1572d7707b73d61b
Diego Zamboni <diego@zzamboni.org>
2017-05-29 10:05:09 +0200

    Chain commands to stop on failure

--- publish_docs.sh
+++ publish_docs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-rm -rf .newdocs
-cp -a docs .newdocs
-git co gh-pages
-mv .newdocs/* .
-rmdir .newdocs
-git add .
-git ci -a -m 'Updated docs'
+rm -rf .newdocs && \
+cp -a docs .newdocs && \
+git co gh-pages && \
+mv .newdocs/* . && \
+rmdir .newdocs && \
+git add . && \
+git ci -a -m 'Updated docs' && \
 git push
 
